2977546f02f1d2a42c4c3ec4594b56d38bc659fb
Иван Момот <momotis2007@gmail.com>
2023-01-15 18:21:19 +0200

    Добавьте файлы проекта.

---
+++ ShapeWF/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ShapeWF
+{
+    public class Circle: Shape
+    {
+        Graphics g;
+        Brush Brush = Brushes.Blue;
+        public Point O { get;set;}
+        public double Width { get; set; }
+        public double  Height { get; set; }
+
+        public override void Draw() => g.FillEllipse(Brush, O, Width, Height);
+    }
+}
---
+++ ShapeWF/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ShapeWF
+{
+    public class Rectangle:Shape
+    {
+        Graphics g;
+        Brush Brush = Brushes.Red; 
+        public Point A { get; set; }
+        public Point B { get; set; }
+        public Point C { get; set; }
+        public Point D { get; set; }
+
+        public override void Draw() => g.FillPolygon(Brush, A, B, C, D);
+    }
+}
